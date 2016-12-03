26f2edaf0d75017827d2804d0cee8ef00f289e16
Szymon <szymon.gwozdz@outlook.com>
2016-12-03 13:37:25 +0100

    change access rights

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-var express = require('express');  
-var graphQLHTTP = require('express-graphql'); 
+var express = require('express');
+var graphQLHTTP = require('express-graphql');
 var Schema = require('./schema.js');
 
 var app = express();
-app.use('/graphql', graphQLHTTP({ 
-    schema: Schema, 
+app.use('/graphql', graphQLHTTP((req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  return {
+    schema: Schema,
     graphiql: true,
-   }));
-  app.listen(4000, function (err) {
-    console.log('GraphQL Server is now running on localhost:4000');
-  });
+  }
+}));
+app.listen(4000, function (err) {
+  console.log('GraphQL Server is now running on localhost:4000');
+});
